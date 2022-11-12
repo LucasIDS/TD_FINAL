c923407f2ccbc8019a0bc6dcb3b9422d44dcbf56
hichemKARFA <hichem.karfa_etudiant@outlook.fr>
2022-11-12 13:56:53 +0100

    TP1 V2

--- src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/App.java
+++ src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/App.java
@@ -6,43 +6,50 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 /**
- * Classe principale de l'application 
+ * Classe principale de l'application
  * s'appuie sur javafx pour le rendu
  */
-public class App extends Application 
+public class App extends Application
 {
-	
+
 	/**
 	 * En javafx start() lance l'application
 	 *
 	 * On cree le SceneGraph de l'application ici
-	 * @see http://docs.oracle.com/javafx/2/scenegraph/jfxpub-scenegraph.htm
-	 * 
+	 * @see <a href="http://docs.oracle.com/javafx/2/scenegraph/jfxpub-scenegraph.htm">...</a>
+	 *
 	 */
 	@Override
-	public void start(Stage stage) throws Exception 
+	public void start(Stage stage) throws Exception
 	{
 		// Nom de la fenetre
-        stage.setTitle("BalleAuPrisonnier");
+		stage.setTitle("BalleAuPrisonnier");
 
-        Group root = new Group();
-        Scene scene = new Scene( root );
+		Group root = new Group();
+		Scene scene = new Scene( root );
 
-        // On cree le terrain de jeu et on l'ajoute a la racine de la scene
-        Field gameField = new Field(scene, 600, 600 );
-        root.getChildren().add( gameField );
-		root.getChildren().add(gameField.getJoueurs()[0].sprite);
-		root.getChildren().add(gameField.getJoueurs()[1].sprite);
 
-        // On ajoute la scene a la fenetre et on affiche
-        stage.setScene( scene );
-        stage.show();
+		// On cree le terrain de jeu et on l'ajoute a la racine de la scene
+		Field gameField = new Field(scene, 600, 600 );
+		root.getChildren().add( gameField );
+		root.getChildren().add(gameField.getJoueurs(1)[0].sprite);
+		root.getChildren().add(gameField.getJoueurs(2)[0].sprite);
+		root.getChildren().add(gameField.getJoueurs(1)[1].sprite);
+		root.getChildren().add(gameField.getJoueurs(2)[1].sprite);
+		root.getChildren().add(gameField.getJoueurs(1)[2].sprite);
+		root.getChildren().add(gameField.getJoueurs(2)[2].sprite);
+
+
+		// On ajoute la scene a la fenetre et on affiche
+		stage.setScene( scene );
+		stage.show();
 	}
-	
-    public static void main(String[] args) 
-    {
-        //System.out.println( "Hello World!" );
-    	Application.launch(args);
-    }
+
+	public static void main(String[] args)
+	{
+		//System.out.println( "Hello World!" );
+		Application.launch(args);
+	}
 }
--- src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Field.java
+++ src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Field.java
@@ -15,142 +15,180 @@ import javafx.scene.paint.Color;
  * Classe gerant le terrain de jeu.
  * 
  */
+
+
+/**
+ * Classe gerant le terrain de jeu.
+ *
+ */
 public class Field extends Canvas {
-	
+
 	/** Joueurs */
-	Player [] joueurs = new Player[2];
+	Player [] team1 = new Player[3];
+	Player [] team2 = new Player[3];
 	/** Couleurs possibles */
 	String[] colorMap = new String[] {"blue", "green", "orange", "purple", "yellow"};
 	/** Tableau traçant les evenements */
-    ArrayList<String> input = new ArrayList<String>();
-    
+	ArrayList<String> input = new ArrayList<String>();
 
-    final GraphicsContext gc;
-    final int width;
-    final int height;
-    
-    /**
-     * Canvas dans lequel on va dessiner le jeu.
-     * 
-     * @param scene Scene principale du jeu a laquelle on va ajouter notre Canvas
-     * @param w largeur du canvas
-     * @param h hauteur du canvas
-     */
-	public Field(Scene scene, int w, int h) 
+
+	final GraphicsContext gc;
+	final int width;
+	final int height;
+
+	/**
+	 * Canvas dans lequel on va dessiner le jeu.
+	 *
+	 * @param scene Scene principale du jeu a laquelle on va ajouter notre Canvas
+	 * @param w largeur du canvas
+	 * @param h hauteur du canvas
+	 */
+	public Field(Scene scene, int w, int h)
 	{
-		super(w, h); 
+		super(w, h);
 		width = w;
 		height = h;
-		
+
 		/** permet de capturer le focus et donc les evenements clavier et souris */
 		this.setFocusTraversable(true);
-		
-        gc = this.getGraphicsContext2D();
-        
-        /** On initialise le terrain de jeu */
-    	joueurs[0] = new Player(gc, colorMap[0], w/2, h-50, "bottom");
-    	joueurs[0].display();
 
-    	joueurs[1] = new Player(gc, colorMap[1], w/2, 20, "top");
-    	joueurs[1].display();
+		gc = this.getGraphicsContext2D();
+
+		/** On initialise le terrain de jeu */
+		team1[0] = new Player(gc, colorMap[0], w/2, h-50, "bottom");
+		team1[0].display();
+
+		team1[1] = new IA(gc, colorMap[0], w/2, h-50, "bottom");
+		team1[1].display();
+
+		team1[2] = new IA(gc, colorMap[0], w/2, h-50, "bottom");
+		team1[2].display();
+
+		team2[0] = new Player(gc, colorMap[1], w/2, 20, "top");
+		team2[0].display();
+
+		team2[1] = new IA(gc, colorMap[1], w/2, 20, "top");
+		team2[1].display();
+
+		team2[2] = new IA(gc, colorMap[1], w/2, 20, "top");
+		team2[2].display();
 
 
-	    /** 
-	     * Event Listener du clavier 
-	     * quand une touche est pressee on la rajoute a la liste d'input
-	     *   
-	     */
-	    this.setOnKeyPressed(
-	    		new EventHandler<KeyEvent>()
-	    	    {
-	    	        public void handle(KeyEvent e)
-	    	        {
-	    	            String code = e.getCode().toString();
-	    	            // only add once... prevent duplicates
-	    	            if ( !input.contains(code) )
-	    	                input.add( code );
-	    	        }
-	    	    });
-
-	    /** 
-	     * Event Listener du clavier 
-	     * quand une touche est relachee on l'enleve de la liste d'input
-	     *   
-	     */
-	    this.setOnKeyReleased(
-	    	    new EventHandler<KeyEvent>()
-	    	    {
-	    	        public void handle(KeyEvent e)
-	    	        {
-	    	            String code = e.getCode().toString();
-	    	            input.remove( code );
-	    	        }
-	    	    });
-	    
-	    /** 
-	     * 
-	     * Boucle principale du jeu
-	     * 
-	     * handle() est appelee a chaque rafraichissement de frame
-	     * soit environ 60 fois par seconde.
-	     * 
-	     */
-	    new AnimationTimer() 
-	    {
-	        public void handle(long currentNanoTime)
-	        {	 
-	            // On nettoie le canvas a chaque frame
-	            gc.setFill( Color.LIGHTGRAY);
-	            gc.fillRect(0, 0, width, height);
-	        	
-	            // Deplacement et affichage des joueurs
-	        	for (int i = 0; i < joueurs.length; i++) 
-	    	    {
-	        		if (i==0 && input.contains("LEFT"))
-	        		{
-	        			joueurs[i].moveLeft();
-	        		} 
-	        		if (i==0 && input.contains("RIGHT")) 
-	        		{
-	        			joueurs[i].moveRight();	        			
-	        		}
-	        		if (i==0 && input.contains("UP"))
-	        		{
-	        			joueurs[i].turnLeft();
-	        		} 
-	        		if (i==0 && input.contains("DOWN")) 
-	        		{
-	        			joueurs[i].turnRight();	        			
-	        		}
-	        		if (i==1 && input.contains("A"))
-	        		{
-	        			joueurs[i].moveLeft();
-	        		} 
-	        		if (i==1 && input.contains("D")) 
-	        		{
-	        			joueurs[i].moveRight();	        			
-	        		}
-	        		if (i==1 && input.contains("W"))
-	        		{
-	        			joueurs[i].turnLeft();
-	        		} 
-	        		if (i==1 && input.contains("S")) 
-	        		{
-	        			joueurs[i].turnRight();	        			
-	        		}
-	        		if (input.contains("SPACE")){
-	        			joueurs[i].shoot();
+		/**
+		 * Event Listener du clavier
+		 * quand une touche est pressee on la rajoute a la liste d'input
+		 *
+		 */
+		this.setOnKeyPressed(
+				new EventHandler<KeyEvent>()
+				{
+					public void handle(KeyEvent e)
+					{
+						String code = e.getCode().toString();
+						// only add once... prevent duplicates
+						if ( !input.contains(code) )
+							input.add( code );
 					}
+				});
 
-	        		
-	        		joueurs[i].display();
-	    	    }
-	    	}
-	     }.start(); // On lance la boucle de rafraichissement 
-	     
+		/**
+		 * Event Listener du clavier
+		 * quand une touche est relachee on l'enleve de la liste d'input
+		 *
+		 */
+		this.setOnKeyReleased(
+				new EventHandler<KeyEvent>()
+				{
+					public void handle(KeyEvent e)
+					{
+						String code = e.getCode().toString();
+						input.remove( code );
+					}
+				});
+
+		/**
+		 *
+		 * Boucle principale du jeu
+		 *
+		 * handle() est appelee a chaque rafraichissement de frame
+		 * soit environ 60 fois par seconde.
+		 *
+		 */
+		new AnimationTimer()
+		{
+			public void handle(long currentNanoTime)
+			{
+				// On nettoie le canvas a chaque frame
+				gc.setFill( Color.LIGHTGRAY);
+				gc.fillRect(0, 0, width, height);
+
+				// Deplacement et affichage des joueurs
+
+				if (input.contains("LEFT"))
+				{
+					team1[0].moveLeft();
+				}
+				if ( input.contains("RIGHT"))
+				{
+					team1[0].moveRight();
+				}
+				if ( input.contains("UP"))
+				{
+					team1[0].turnLeft();
+				}
+				if ( input.contains("DOWN"))
+				{
+					team1[0].turnRight();
+				}
+
+				if ( input.contains("M"))
+				{
+					team1[0].shoot();
+				}
+
+				team1[0].display();
+
+
+				if (input.contains("Q"))
+				{
+					team2[0].moveLeft();
+				}
+				if (input.contains("D"))
+				{
+					team2[0].moveRight();
+				}
+				if (input.contains("Z"))
+				{
+					team2[0].turnLeft();
+				}
+				if ( input.contains("S"))
+				{
+					team2[0].turnRight();
+				}
+
+				if ( input.contains("SPACE"))
+				{
+					team2[0].shoot();
+				}
+
+				team2[0].display();
+
+			}
+		}.start(); // On lance la boucle de rafraichissement
+
 	}
 
-	public Player[] getJoueurs() {
-		return joueurs;
+	public Player[] getJoueurs(int a) {
+		if (a==1)
+		{
+			return team1;
+		}
+		else if (a==2)
+		{
+			return team2;
+		}
+		else {
+			return new Player[0];
+		}
 	}
 }
